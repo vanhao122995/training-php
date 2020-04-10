1bf25c31bd58dbf33ad3843373683c186047ca2b
vanhao122995 <vuvanhao122995@gmail.com>
2020-04-10 13:31:20 +0700

    10/4

--- 09.oop/DB.php
+++ 09.oop/DB.php
@@ -12,36 +12,39 @@ class DB {
         $dtb_name   = "my_table_product";
         $table      = "product";
         $connect    = @mysqli_connect($servername, $username, $password, $dtb_name);
-        // Change character set to utf8
-        @mysqli_set_charset($connect,"utf8");
+        // Change character set to utf8     
         if($connect){
             $this->conn = $connect;
             $this->table = $table;
+            mysqli_set_charset($connect,"utf8");
         }
 
     }
 
-    public function getAll($select = [], $where = [], $limit = [0, 10]) {
+    public function getAll($params = []) {
         $data = null;
         $sql = '';
         $str_where = '';
         $str_select = '*';
+        $str_limit = '';
+
+        //xử lý điều kiện select
+        if(isset($params['select']) && !empty($params['select'])) {
+            $str_select = implode(',', $params['select']);
+        }
 
         //xử lý điều kiện where
-        if(!empty($where)) {
-            foreach($where as $row) {
-                if(!empty($row)) {
-                    $str_where .= $row[0] . $row[1] . (is_numeric($row[2]) ? $row[2] : '\'' . $row[2] . '\'');                  
-                }
-                $str_where .= ' AND ';
-            }
-            $str_where = 'WHERE ' . $str_where . ' true';
+        if(isset($params['where']) && !empty($params['where'])) {
+            $str_where = $this->reateSqlWhere($params['where']);
         }
-        //xử lý điều kiện select
-        if(!empty($select)) {
-            $str_select = implode(',', $select);
+
+        //xử lý điều kiện limit
+        if(isset($params['limit']) && !empty($params['limit'])) {
+            $param_limit = $params['limit'];
+            $str_limit = 'LIMIT ' . $param_limit[0] . ',' . $param_limit[1];//LIMIT 0, 10
         }
-        $sql = "SELECT $str_select FROM $this->table $str_where LIMIT $limit[0], $limit[1]";
+        //SELECT *(id, name, status) WHERE id = 10 AND name = 'werty' LIMIT 0, n
+        $sql = "SELECT $str_select FROM $this->table $str_where $str_limit";
         $result = mysqli_query ($this->conn, $sql);
         if($result) {
             while($item = mysqli_fetch_object($result)) {
@@ -50,6 +53,53 @@ class DB {
         }
         return $data;
     }
+    //DELETE FROM product WHERE id = 10; key = value 
+    //DLETE FROM product WHERE key IN ('', '2', 3,.....n)
+    //[1, 2, 3 .......n = 1000]
+    public function delete($value) {
+        if(!is_array($value)) {
+            if(!empty($value)) {
+                $sql = "DELETE FROM $this->table WHERE id = $value";
+                return mysqli_query ($this->conn, $sql);
+            }
+        }else {
+            // foreach($value as $val) {
+            //     $sql = "DELETE FROM $this->table WHERE $key = " .  (is_numeric($val) ? $val : '\'' . $val . '\'');
+            //     mysqli_query ($this->conn, $sql);
+            // }
+            $sql = "DELETE FROM $this->table WHERE id IN " . "(" . implode(',', $value) . ")";
+            mysqli_query ($this->conn, $sql);
+        }
+    }
+    //[['id', '=', 123456]]
+    public function isExistRecord($where = []) {
+        //SELECT * FROM user WHERE password = old
+        $str_where = '';
+         //xử lý điều kiện where
+         if(isset($where) && !empty($where)) {
+            $str_where = $this->reateSqlWhere($where);
+            $sql = "SELECT * FROM $this->table $str_where";
+            return mysqli_query ($this->conn, $sql);
+        }
+    }
 
+    public function reateSqlWhere($where = []) {
+        $str_where = '';
+        if(isset($where) && !empty($where)) {
+            foreach($where as $key => $row) {
+                if(!empty($row)) {
+                    $str_where .= $row[0] . $row[1] . (is_numeric($row[2]) ? $row[2] : '\'' . $row[2] . '\'');                  
+                }
+                //1 => 0 key       => 0 => count
+                //2 => 0 1   key   => 1
+                //3 => 0 1 2  key  => 2
+                if(count($where) - 1 != $key) {
+                    $str_where .= ' AND ';
+                }
+                
+            }
+        }
+        return $str_where;
+    }
 
 }
--- 09.oop/dab.php
+++ 09.oop/dab.php
@@ -1,11 +1,19 @@
 <?php
     require_once('./DB.php');
     $db = new DB();
-    
-    $where = array(
-        ['id', '>', 444]
-    );
-    $data = $db->getAll(['id', 'name', 'status'], $where);
-    echo '<pre>';
-    print_r($data);
-    echo '</pre>';die();
+
+    $params = [];
+    //$params['select'] = ['id', 'name', 'status'];
+    // $params['where'] = [
+    //     ['id', '=', 444],
+    // ];
+    //$params['limit'] = [0, 10]; //LIMIT 0, 10
+
+    //$data = $db->getAll();
+    //$db->delete([222263, 222262]);
+    $where = [
+        ['id', '=', 444],
+        ['name', '=', 'ti'],
+    ];
+    $db->isExistRecord($where);
+
